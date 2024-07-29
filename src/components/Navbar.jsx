54bf3eb1e16303960d16dfade772746809c0c604
Sheehab <shehabedinali@gmail.com>
2024-07-29 20:56:31 +0300

    created a Hero component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <div className="flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
 
-            {/* <!-- Logo --> */}
+            {/*  Logo  */}
 
             <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
               <img
