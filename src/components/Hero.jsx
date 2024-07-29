dfdb46d81e863aa9c1c5b94c3a29c392b08589fb
Sheehab <shehabedinali@gmail.com>
2024-07-29 22:05:02 +0300

    added typewriter animation to the hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
   return (
-    <section className="bg-indigo-700 py-20 mb-4">
+    <section className="bg-custom-brown py-20 mb-4">
       <div
         className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
       >
         <div className="text-center">
           <h1
-            className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
+            className="text-4xl font-extrabold text-custom-yellow sm:text-5xl md:text-6xl"
           >
-            Become a React Dev
+            <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Become a React Dev',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Become a Social Media Manager',
+        1000,
+        'Become a Back-End Dev',
+        1000,
+        'Become a Tester',
+        1000,
+        'Become Employed!',
+        2000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ display: 'inline-block' }}
+      repeat={Infinity}
+    />
           </h1>
+          
           <p className="my-4 text-xl text-white">
             Find the React job that fits your skills and needs
           </p>
