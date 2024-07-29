40f989a6a6e8249e2595ee2b8daae78ad9a40c44
Sheehab <shehabedinali@gmail.com>
2024-07-29 22:47:02 +0300

    added a dynamic card component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
+import HomeCards from './components/HomeCards.jsx'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <HomeCards />
     </>
   )
 }
