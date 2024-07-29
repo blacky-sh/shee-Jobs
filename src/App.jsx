bf89a0fa1f0f125054a77f30ee359bcb43cde40c
Sheehab <shehabedinali@gmail.com>
2024-07-29 23:35:37 +0300

    added JobListings component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import HomeCards from './components/HomeCards.jsx'
+import JobListings from './components/JobListings.jsx'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <HomeCards />
+      <JobListings />
     </>
   )
 }
