e92beed539d0c2efd4e6f7fd1b98b9c75282e5f3
Sheehab <shehabedinali@gmail.com>
2024-07-30 11:36:36 +0300

    made the joblistings component to show only 3 jobs in home page but all in Jobs page.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import jobs from '../jobs.json'
 import JobListing from './JobListing'
 
-const JobListings = () => {
+const JobListings = ({isHome = false}) => {
+  const jobListings = isHome ? jobs.slice(0,3) : jobs;
+
   return (
     <section class="bg-blue-50 px-4 py-10">
     <div class="container-xl lg:container m-auto">
@@ -10,7 +12,7 @@ const JobListings = () => {
         Browse Jobs
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {jobs.map((job)=>(
+        {jobListings.map((job)=>(
             <JobListing key={job.id} job={job}/>
         ))}
 
