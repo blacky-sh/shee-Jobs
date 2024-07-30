a174391786e45c850fd6f41e248d80d23ab1dcc9
Sheehab <shehabedinali@gmail.com>
2024-07-30 13:00:55 +0300

    getting json-server set up to fetch data from a json file not an array.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import jobs from '../jobs.json'
+import { useEffect, useState } from 'react';
 import JobListing from './JobListing'
 
 const JobListings = ({isHome = false}) => {
-  const jobListings = isHome ? jobs.slice(0,3) : jobs;
+  // const jobListings = isHome ? jobs.slice(0,3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+        try{
+          const res = await fetch('http://localhost:8000/jobs');
+          const data = await res.json();
+          setJobs(data);
+        }
+        catch(error){
+          console.log('error fetching jobs data', error)
+        }
+        finally{
+          setloading(false);
+        }
+    }
+
+    fetchJobs();
+  }, [])
 
   return (
     <section class="bg-blue-50 px-4 py-10">
@@ -12,10 +32,16 @@ const JobListings = ({isHome = false}) => {
         {isHome ? 'Recent jobs' : 'Browse Jobs'}
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {jobListings.map((job)=>(
+        {loading 
+        ? (<h2>Loading . . .</h2>
+        )
+        : (<>
+        {jobs.map((job)=>(
             <JobListing key={job.id} job={job}/>
         ))}
-
+        </>
+        )
+}
     
     </div>
       </div>
